2f657ab7730c77a9ffe9ee2e7fd5366f308295d7
Bronsberg <G98734@NGC.AUTHORITY>
2022-02-27 18:09:20 -0600

    Initial commit

---
+++ Navigation.py
@@ -0,0 +1,309 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Navigation
+# 
+# ---
+# 
+# In this notebook, you will learn how to use the Unity ML-Agents environment for the first project of the [Deep Reinforcement Learning Nanodegree](https://www.udacity.com/course/deep-reinforcement-learning-nanodegree--nd893).
+# 
+# ### 1. Start the Environment
+# 
+# We begin by importing some necessary packages.  If the code cell below returns an error, please revisit the project instructions to double-check that you have installed [Unity ML-Agents](https://github.com/Unity-Technologies/ml-agents/blob/master/docs/Installation.md) and [NumPy](http://www.numpy.org/).
+
+# In[1]:
+
+
+from unityagents import UnityEnvironment
+import numpy as np
+
+
+# Next, we will start the environment!  **_Before running the code cell below_**, change the `file_name` parameter to match the location of the Unity environment that you downloaded.
+# 
+# - **Mac**: `"path/to/Banana.app"`
+# - **Windows** (x86): `"path/to/Banana_Windows_x86/Banana.exe"`
+# - **Windows** (x86_64): `"path/to/Banana_Windows_x86_64/Banana.exe"`
+# - **Linux** (x86): `"path/to/Banana_Linux/Banana.x86"`
+# - **Linux** (x86_64): `"path/to/Banana_Linux/Banana.x86_64"`
+# - **Linux** (x86, headless): `"path/to/Banana_Linux_NoVis/Banana.x86"`
+# - **Linux** (x86_64, headless): `"path/to/Banana_Linux_NoVis/Banana.x86_64"`
+# 
+# For instance, if you are using a Mac, then you downloaded `Banana.app`.  If this file is in the same folder as the notebook, then the line below should appear as follows:
+# ```
+# env = UnityEnvironment(file_name="Banana.app")
+# ```
+
+# In[2]:
+
+
+env = UnityEnvironment(file_name="Banana_Windows_x86_64/Banana.exe")
+
+
+# Environments contain **_brains_** which are responsible for deciding the actions of their associated agents. Here we check for the first brain available, and set it as the default brain we will be controlling from Python.
+
+# In[3]:
+
+
+# get the default brain
+brain_name = env.brain_names[0]
+brain = env.brains[brain_name]
+
+
+# ### 2. Examine the State and Action Spaces
+# 
+# The simulation contains a single agent that navigates a large environment.  At each time step, it has four actions at its disposal:
+# - `0` - walk forward 
+# - `1` - walk backward
+# - `2` - turn left
+# - `3` - turn right
+# 
+# The state space has `37` dimensions and contains the agent's velocity, along with ray-based perception of objects around agent's forward direction.  A reward of `+1` is provided for collecting a yellow banana, and a reward of `-1` is provided for collecting a blue banana. 
+# 
+# Run the code cell below to print some information about the environment.
+
+# In[4]:
+
+
+# reset the environment
+env_info = env.reset(train_mode=True)[brain_name]
+
+# number of agents in the environment
+print('Number of agents:', len(env_info.agents))
+
+# number of actions
+action_size = brain.vector_action_space_size
+print('Number of actions:', action_size)
+
+# examine the state space 
+state = env_info.vector_observations[0]
+print('States look like:\n', state)
+state_size = len(state)
+print('States have length:', state_size)
+
+
+# ### 3. Take Random Actions in the Environment
+# 
+# In the next code cell, you will learn how to use the Python API to control the agent and receive feedback from the environment.
+# 
+# Once this cell is executed, you will watch the agent's performance, if it selects an action (uniformly) at random with each time step.  A window should pop up that allows you to observe the agent, as it moves through the environment.  
+# 
+# Of course, as part of the project, you'll have to change the code so that the agent is able to use its experience to gradually choose better actions when interacting with the environment!
+
+# In[6]:
+
+
+env_info = env.reset(train_mode=False)[brain_name] # reset the environment
+state = env_info.vector_observations[0]            # get the current state
+score = 0                                          # initialize the score
+while True:
+    action = np.random.randint(action_size)        # select an action
+    env_info = env.step(action)[brain_name]        # send the action to the environment
+    next_state = env_info.vector_observations[0]   # get the next state
+    reward = env_info.rewards[0]                   # get the reward
+    done = env_info.local_done[0]                  # see if episode has finished
+    score += reward                                # update the score
+    state = next_state                             # roll over the state to next time step
+    if done:                                       # exit loop if episode finished
+        break
+    
+print("Score: {}".format(score))
+
+
+# When finished, you can close the environment.
+
+# In[7]:
+
+
+env.close()
+
+
+# ### 4. It's Your Turn!
+# 
+# Now it's your turn to train your own agent to solve the environment!  When training the environment, set `train_mode=True`, so that the line for resetting the environment looks like the following:
+# ```python
+# env_info = env.reset(train_mode=True)[brain_name]
+# ```
+
+# #### 4a. Define Network
+# First we define our neural network to represent our Q values.
+
+# In[1]:
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class QNetwork(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, seed, fc1_units=128, fc2_units=64):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+            fc1_units (int): Number of nodes in first hidden layer
+            fc2_units (int): Number of nodes in second hidden layer
+        """
+        super(QNetwork, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        "*** YOUR CODE HERE ***"
+        self.fc1 = nn.Linear(state_size, fc1_units)     # input state_size (37), output 128
+        self.fc2 = nn.Linear(fc1_units, fc2_units)      # input 128, output 64
+        self.fc3 = nn.Linear(fc2_units, action_size)    # input 64, output action_size (4)
+
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        return self.fc3(x)
+
+
+# #### 4b. Test Agent
+# We will test our unintialized agent to verify our input/output with the Unity environment.
+
+# In[23]:
+
+
+from agent import Agent
+
+agent = Agent(state_size=37, action_size=4, seed=0)
+
+env_info = env.reset(train_mode=False)[brain_name] # reset the environment
+state = env_info.vector_observations[0]
+
+# try the untrained agent
+score = 0                                          # initialize the score
+while True:
+    action = np.int32(agent.act(state))            # select an action using our new agent
+    env_info = env.step(action)[brain_name]        # send the action to the environment
+    next_state = env_info.vector_observations[0]   # get the next state
+    reward = env_info.rewards[0]                   # get the reward
+    done = env_info.local_done[0]                  # see if episode has finished
+    score += reward                                # update the score
+    state = next_state                             # roll over the state to next time step
+    if done:                                       # exit loop if episode finished
+        break
+    
+print("Score: {}".format(score))
+
+
+# In[10]:
+
+
+env.close()
+
+
+# ### 4c. Train the Agent with Deep-Q Learning
+
+# When restarting the notebook we can start from here.
+
+# In[2]:
+
+
+from unityagents import UnityEnvironment
+import numpy as np
+
+env = UnityEnvironment(file_name="Banana_Windows_x86_64/Banana.exe")
+brain_name = env.brain_names[0]
+brain = env.brains[brain_name]
+
+
+# In[5]:
+
+
+from collections import deque
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+from agent import Agent
+
+agent = Agent(state_size=37, action_size=4, seed=0)
+
+def dqn(n_episodes=2000, max_t=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.995):
+    """Deep Q-Learning.
+    
+    Params
+    ======
+        n_episodes (int): maximum number of training episodes
+        max_t (int): maximum number of timesteps per episode
+        eps_start (float): starting value of epsilon, for epsilon-greedy action selection
+        eps_end (float): minimum value of epsilon
+        eps_decay (float): multiplicative factor (per episode) for decreasing epsilon
+    """
+    scores = []                        # list containing scores from each episode
+    scores_window = deque(maxlen=100)  # last 100 scores
+    eps = eps_start                    # initialize epsilon
+    
+    for i_episode in range(1, n_episodes+1):
+        env_info = env.reset(train_mode=True)[brain_name] # reset the environment
+        state = env_info.vector_observations[0]   #state = env.reset()
+        score = 0
+        for t in range(max_t):
+            action = np.int32(agent.act(state, eps))
+            env_info = env.step(action)[brain_name]        # send the action to the environment
+            next_state = env_info.vector_observations[0]   # get the next state
+            reward = env_info.rewards[0]                   # get the reward
+            done = env_info.local_done[0]                  # see if episode has finished
+            #next_state, reward, done, _ = env.step(action)[brain_name]
+            agent.step(state, action, reward, next_state, done)
+            state = next_state
+            score += reward
+            if done:
+                break 
+        scores_window.append(score)       # save most recent score
+        scores.append(score)              # save most recent score
+        eps = max(eps_end, eps_decay*eps) # decrease epsilon
+        print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)), end="")
+        if i_episode % 100 == 0:
+            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
+        if np.mean(scores_window)>=16.0:
+            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, np.mean(scores_window)))
+            torch.save(agent.qnetwork_local.state_dict(), 'checkpoint.pth')
+            break
+    return scores
+
+scores = dqn()
+
+# plot the scores
+fig = plt.figure()
+ax = fig.add_subplot(111)
+plt.plot(np.arange(len(scores)), scores)
+plt.ylabel('Score')
+plt.xlabel('Episode #')
+plt.show()
+
+
+# ### 4d. Watch the Agent
+
+# In[7]:
+
+
+# load the weights from file
+agent.qnetwork_local.load_state_dict(torch.load('checkpoint.pth'))
+
+env_info = env.reset(train_mode=False)[brain_name] # reset the environment
+state = env_info.vector_observations[0]
+
+score = 0  # initialize the score
+while True:
+    action = np.int32(agent.act(state))            # select an action using our new agent
+    env_info = env.step(action)[brain_name]        # send the action to the environment
+    next_state = env_info.vector_observations[0]   # get the next state
+    reward = env_info.rewards[0]                   # get the reward
+    done = env_info.local_done[0]                  # see if episode has finished
+    score += reward                                # update the score
+    state = next_state                             # roll over the state to next time step
+    if score >= 13:                                # exit loop if episode finished
+        break
+    
+print("Score: {}".format(score))
+
+
+# In[8]:
+
+
+env.close()
+
--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # Bananavigation
-Using deep reinforcement learning to collect bananas in an ML-Agents Unity environment
+A deep reinforcement learning implementation to collect bananas in an ML-Agents Unity environment, built for the Udacity nanodegree program.
+
+# Project 1: Navigation
+
+### Introduction
+
+This project trains an agent to navigate and collect bananas in a large, square world set up within the ML-Agents Unity environment.
+
+A reward of +1 is provided for collecting a yellow banana, and a reward of -1 is provided for collecting a blue banana. The goal is to have the agent collect as many yellow bananas as possible while avoiding blue bananas.
+
+The state space has 37 dimensions and contains the agent's velocity, along with ray-based perception of objects around agent's forward direction. Given this information, the agent has to learn how to best select actions. Four discrete actions are available, corresponding to:
+- **`0`** - move forward.
+- **`1`** - move backward.
+- **`2`** - turn left.
+- **`3`** - turn right.
+
+The task is episodic, and the environment is considered solved when the agent gets an average score of +13 over 100 consecutive episodes.
+
+### Getting Started
+
+1. Download the OS-appropriate environment from one of the links below:
+    - Linux: [click here](https://s3-us-west-1.amazonaws.com/udacity-drlnd/P1/Banana/Banana_Linux.zip)
+    - Mac OSX: [click here](https://s3-us-west-1.amazonaws.com/udacity-drlnd/P1/Banana/Banana.app.zip)
+    - Windows (32-bit): [click here](https://s3-us-west-1.amazonaws.com/udacity-drlnd/P1/Banana/Banana_Windows_x86.zip)
+    - Windows (64-bit): [click here](https://s3-us-west-1.amazonaws.com/udacity-drlnd/P1/Banana/Banana_Windows_x86_64.zip)
+
+    (_For Windows users_) Check out [this link](https://support.microsoft.com/en-us/help/827218/how-to-determine-whether-a-computer-is-running-a-32-bit-version-or-64) if you need help with determining if your computer is running a 32-bit version or 64-bit version of the Windows operating system.
+
+    (_For AWS_) If you'd like to train the agent on AWS (and have not [enabled a virtual screen](https://github.com/Unity-Technologies/ml-agents/blob/master/docs/Training-on-Amazon-Web-Service.md)), then please use [this link](https://s3-us-west-1.amazonaws.com/udacity-drlnd/P1/Banana/Banana_Linux_NoVis.zip) to obtain the environment.
+
+2. Clone Udacity's Deep Reinforcment Learning Nanodegree repository and place the file in this pository, in the `p1_navigation/` folder, and unzip (or decompress) the file. The repository is available at [https://github.com/udacity/deep-reinforcement-learning](https://github.com/udacity/deep-reinforcement-learning).
+
+### Instructions
+
+Open the `Navigation.ipynb` Jupyter notebook and follow the instructions in the cells to train and test the agent.
